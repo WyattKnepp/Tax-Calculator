75f94727f053f2b2ab99f29a44df96323fb202b9
Wyatt Knepp <wyattjoeknepp@gmail.com>
2021-08-04 11:10:40 -0500

    Initial Commit

---
+++ Tax Calculator/Tax_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tax_CalculatorApp.swift
+//  Tax Calculator
+//
+//  Created by Wyatt Knepp on 8/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Tax_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
